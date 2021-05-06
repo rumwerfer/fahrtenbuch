bd323c1af66f01f589f3d0bd7e397adecdb3825e
rumwerfer <git@hornsteiner.de>
2021-05-06 02:45:04 +0200

    camera overlay

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 import {
   SafeAreaView,
+  View,
   Text,
+  Button,
 } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import Strings from '../res/Strings.js';
+import Colors from '../res/Colors.js';
+
+const CameraOverlay = () => {
+  return (
+    <View>
+      <View style={{width: '100%', height: 100, backgroundColor: Colors.black, }}/>
+      <View style={{flexDirection: 'row', height: 100}}>
+        <View style={{flex: .07, backgroundColor: Colors.black}} />
+        <View style={{flex: .7, borderWidth: 2, borderColor: Colors.green, }} />
+        <View style={{flex: .23, backgroundColor: Colors.black, alignItems: 'center', justifyContent: 'center'}}>
+          <Button title='ok' color={Colors.green} />
+        </View>
+      </View>
+      <View style={{height: '100%', backgroundColor: Colors.black, }}/>
+    </View>
+  );
+}
 
 class CameraScreen extends Component {
   render() {
     return (
-      <SafeAreaView style={{ flex: 1}}>
+      <SafeAreaView style={{flex: 1}}>
         <RNCamera
           ref={ref => {
             this.camera = ref;
@@ -23,7 +42,9 @@ class CameraScreen extends Component {
           }}
           captureAudio={false}
           style={{flex: 1}}
-        />
+        >
+          <CameraOverlay style={{flex: 1}}/>
+        </RNCamera>
       </SafeAreaView>
     );
   }
